885fc06e6dedb9f45951c85678ff90834fec5650
bhavyanshu <bhavyanshu.spl@gmail.com>
2013-06-17 16:05:11 +0530

    Initializing repo for Java Tutorials

---
+++ LearnJava/src/HelloWorld.java
@@ -0,0 +1,38 @@
+/**
+ * @author Bhavyanshu Parasher - bhavyanshu.spl@gmail.com
+ * 
+ * Hello all, this is the first tutorial for Java. Simply go on reading through the code.
+ * I have added line by line comments to each code to explain it in a simpler manner.
+ * If you have any query then simple contact me bhavyanshu.spl@gmail.com
+ */
+
+public class HelloWorld { //We have defined a class HelloWorld which has "Public" access which in simple term means that it is accessible from anywhere.
+	
+	/*
+	 * @param args
+	 * 
+	 * Okay now let us look at this line -> public static void main(){}
+	 * public means that anyone can access it. It's like an access mode. Others are private, protected which we will discuss later.    
+	 * static means that you can run this method without creating an instance of Main.
+	 * void means that this method doesn't return any value. Or else we must use the `return "something";` if in case it is supposed to return anything. 
+	 * main is the name of the method. 
+	 * 
+	 * Now let us look at the statement inside the eclosed brackers -> `String[] args`
+	 * We need to pass some arguements to a method we define. In this case it's an array of strings.
+	 */
+	public static void main(String[] args){
+		System.out.println("Hello World"); 
+		/*
+		 * System is a pre-defined class that Java provides us and it holds some useful methods and variables. You should refer to java docs to know more.
+		 * out is a static variable in within System class that basically holds the output.
+		 * 
+		 * Now remember two things here. 
+		 * if you use System.out.println(), the cursor skips the line and moves to next line after displaying required result.
+		 * if you use System.out.print(), the cursor remain on the same line after displaying the result.
+		 */
+	}
+}
+
+/**
+ * Expected Output : Hello World
+ */
---
+++ README.md
@@ -0,0 +1,13 @@
+Java-Tutorials
+==============
+
+This repo has java code and it properly commented out. I have explained each and every string i code. I will keep updating this with new code. Read the README file for more information.
+
+I will be posting java tutorials over here with complete code and discription of every single word i code.
+Not because i am free but it will also help me improve my command over this language. 
+
+You are free to clone, fork, contribute, distribute under the following simple terms:
+1) You must keep the name of the author of the file as it is. Do not modify it.
+2) In case of multiple authors, seperate the names with comma ex-> John, Jack, Jill and so on.
+
+That's all. Respect free education. Cheers!
